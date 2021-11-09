fdef2b270b7d5dc3804f996db9456c4663db3d4b
khangbeo <khangbeo2012@gmail.com>
2021-11-08 20:48:15 -0800

    add files to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,12 @@
+// dotenv is used to load the .env file
+// required if making use of the .env file
 require("dotenv").config();
 const {
   NODE_ENV = "development",
   DEVELOPMENT_DATABASE_URL,
   PRODUCTION_DATABASE_URL,
 } = process.env;
+// if NODE_ENV is production then URL will be PRODUCTION_DATABASE_URL
 const URL =
   NODE_ENV === "production"
     ? PRODUCTION_DATABASE_URL
